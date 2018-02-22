dd87e2196faa00b23cd1c0a3b105dd85cd87aa05
Leblayd <leblaydavid7@gmail.com>
2018-02-22 16:50:31 +0100

    added js to load index table data

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,41 @@
+// Mustache example:
+// Mustache.render("{{a}}", {a: "x"});
+// returns "x"
+
+function appendUnitToLargeNumber(num) {
+    let units = ['', ' Thousand', ' Million', ' Billion', ' Trillion', 'Quadrillion', 'Quintillion'];
+    let unitIndex = 0;
+
+    // Divide the number by 1000 as long as it stays above 1
+    while ( !Math.floor(num / 1000) < 1) {
+        unitIndex++;
+        num = num / 1000;
+    }
+    return num.toString() + units[unitIndex];
+}
+
+
 function loadDataToTable(response) {
     let data = response.results;
-    console.log(data);
+
+    $('#loading-placeholder').remove();
+    $.each(data, function(index, object) {
+        object.population = appendUnitToLargeNumber(object.population);
+        if (object.surface_water !== 'unknown') {
+            object.surface_water += '%';
+        }
+        let tableRow =
+            `<tr>
+                <td>{{ name }}</td>
+                <td>{{ diameter }}</td>
+                <td>{{ climate }}</td>
+                <td>{{ terrain }}</td>
+                <td>{{ surface_water }}</td>
+                <td>{{ population }}</td>
+            </tr>`;
+        tableRow = Mustache.render(tableRow, object);
+        $('#index-table').append(tableRow);
+    });
 }
 
 
@@ -12,6 +47,8 @@ function getPlanetsData () {
 function onLoadIndex() {
     getPlanetsData();
 }
+
+
 
 
 $(document).ready(onLoadIndex);
