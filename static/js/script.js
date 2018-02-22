6939c20f06f5a6f50e985962d22a44c9a6ce62e4
Leblayd <leblaydavid7@gmail.com>
2018-02-22 15:52:54 +0100

    base JS to get api data

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,17 @@
-console.log('hi');
+function loadDataToTable(response) {
+    let data = response.results;
+    console.log(data);
+}
+
+
+function getPlanetsData () {
+    $.getJSON('https://swapi.co/api/planets', loadDataToTable);
+}
+
+
+function onLoadIndex() {
+    getPlanetsData();
+}
+
+
+$(document).ready(onLoadIndex);
